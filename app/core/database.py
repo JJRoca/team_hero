7110be65a3fccde35226f9bdf7aeac0eadc8b5c5
JJRoca <hanzoblade1234@outlook.com>
2024-09-17 17:53:47 -0400

    alembic migrations

--- app/core/database.py
+++ app/core/database.py
@@ -1,12 +1,15 @@
 #from models.hero import Hero
 #from models.team import Team
+import dotenv.variables
 from sqlmodel import SQLModel, create_engine
 import dotenv
 import os
 dotenv.load_dotenv()
 DATABASE_URL=os.getenv("DATABASE_URL")
 
-engine= create_engine(DATABASE_URL, echo= True)
-print(DATABASE_URL)
+engine= create_engine(DATABASE_URL,echo= True)
+
+#print(DATABASE_URL)
+
 def create_db_tables():
     SQLModel.metadata.create_all(engine)
